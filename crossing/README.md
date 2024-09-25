711351bd7d0ac8321fe5ddb145eb3aa31f2e8cc1
Polarace <adam@beyer.cz>
2024-09-25 20:42:10 +0200

    Finished crossing for production, modified other

--- crossing/README.md
+++ crossing/README.md
@@ -6,63 +6,12 @@ A level crossing sign. based on czech ažd-97.
   <img src="./doc/crossing-video.gif">
 </p>
 
-### Features 
 
-- 2 selectable operational modes (changeable using solder bridges)
-    1) the leds blinking alternately
-    2) pad in the middle can be used as a button to turn the leds on
-- can be soldered with smd or tht parts.
-- a printable leg (M2 screws for mounting)
- 
-### Mounting
+### Contents
 
-1. On top of the board is a hole for a lanyard/keychain.
-2. In the folder `3D addons` are STLs for a leg and hoods for the LEDs. 
-    - use an M2 screw
-    - No nut is necessary, the leg is made to be a tight fit
-
-### Circuit
-
-[falstad link](https://tinyurl.com/22jwxxro)
-
-the basic circuit is just a basic astable multivibrator with 2 leds. 
-
-the second circuit bypasses the capacitors, instead using a resistive pad and a resistor divider, which pull both bases of the transistors high, thus turning led's on.
-
-you don't need to solder every component for only one of the circuits to work. all parts are labeled with the numbers **1** or **2**, each corresponding to one of the circuits. you can then select the desired one using solder pads.
-
-<p align="center">
-  <img width="400" height="300" src="./doc/sld-numbers.png">
-</p>
-
-### Schematic
-
-<p align="center">
-  <img src="./doc/sch.png">
-</p>
-
-### PCB + dimensions
-
-<p align="center">
-  <img src="./doc/pcb.png">
-</p>
-
-### BOM
-| REF | CNT | NAME
-| --- | ----- | -----
-| BT1 | 1 | 2032 Holder
-| C1, C2 | 2 | 47uF, 6.3x5
-| D1, D2 | 2 | Red LED, 10mm
-| Q1, Q2 | 2 | 2N3904, TO-92
-| R1, R6 | 2 | 62k, 0207
-| R2, R5 | 2 | 100R, 0207
-| R3 | 1 | 1M, 0207
-| R4 | 1 | 10k, 0207
-| SW1 | 1 | 3-Pin, SPDT
-| - | - | -
-| SUM | 14 | 
-
-#### Disclaimer 
-I hereby give up all responsibility for the functionality of your boards.
-
-
+| folder | contents |
+| ------ | -------- |
+| [crossing-blink/](/crossing/crossing-blink/) | only blinks
+| [crossing-dual/](/crossing/crossing-dual/) | lights up or blinks
+| [3D Addons](/crossing/3D addons/) | printable leg and signal visors
+| [doc](/crossing/doc/) | images/gifs
--- mddm-badge/fw/main.c
+++ mddm-badge/fw/main.c
@@ -26,8 +26,6 @@
 
 #define _XTAL_FREQ 2000000
 
-#include <builtins.h>
-
 // This means, that max ratio of on/off time is 64:1
 // although 16 would be enough, I found it better with 64 and then div by 4
 #define PWM_RESOLUTION 64
@@ -46,73 +44,62 @@ uint8_t sw_pwm_duty = 1;
 // Periph. init
 // ============
 
-void pwm_init(void)
-{
+void pwm_init(void) {
 
-    PWM1CON = 0x0; // Clear PWM1CON
-    PWM2CON = 0x0; // Clear PWM2CON
+  PWM1CON = 0x0; // Clear PWM1CON
+  PWM2CON = 0x0; // Clear PWM2CON
 
-    PR2 = PWM_RESOLUTION - 1;
-    // Enable PWM Module, Module Output
-    PORTA = 0x3;
-    PWM1CON = 0b11010000;
-    PWM2CON = 0b11010000;
+  PR2 = PWM_RESOLUTION - 1;
+  // Enable PWM Module, Module Output
+  PORTA = 0x3;
+  PWM1CON = 0b11010000;
+  PWM2CON = 0b11010000;
 
-    // Clear duty cycle registers
-    PWM1DCH = 0x0;
-    PWM1DCL = 0x0;
-    PWM2DCH = 0x0;
-    PWM2DCL = 0x0;
+  // Clear duty cycle registers
+  PWM1DCH = 0x0;
+  PWM1DCL = 0x0;
+  PWM2DCH = 0x0;
+  PWM2DCL = 0x0;
 
-    // Set prescaler to 1, turn timer on
-    // T2CON = 0b100; //(1 << 2);
-    T2CONbits.TMR2ON = 1;
+  // Set prescaler to 1, turn timer on
+  // T2CON = 0b100; //(1 << 2);
+  T2CONbits.TMR2ON = 1;
 
-    // Enable the pwm pin output
-    TRISA = 0x0;
+  // Enable the pwm pin output
+  TRISA = 0x0;
 
-    // Set duty cycle to 40
-    PWM1DCH = 0b00001010;
-    PWM1DCL = 0b00000000;
-    PWM2DCH = 0b00001010;
-    PWM2DCL = 0b00000000;
+  // Set duty cycle to 40
+  PWM1DCH = 0b00001010;
+  PWM1DCL = 0b00000000;
+  PWM2DCH = 0b00001010;
+  PWM2DCL = 0b00000000;
 
-    return;
+  return;
 }
 
-void interrupt_init(void)
-{
-    // for sw pwm
-    PIE1bits.TMR2IE = 1;
+void interrupt_init(void) {
+  // for sw pwm
+  PIE1bits.TMR2IE = 1;
 
-    // button on positive edge
-    IOCAPbits.IOCAP3 = 1;
+  // button on positive edge
+  IOCAPbits.IOCAP3 = 1;
 
-    // Intcon: PEIE, IOCIE, GIE
-    INTCON = 0b11001000;
-    // INTCONbits.PEIE = 1;
-    // INTCONbits.IOCIE = 1;
-    // INTCONbits.GIE = 1;
+  // Intcon: PEIE, IOCIE, GIE
+  INTCON = 0b11001000;
+  // INTCONbits.PEIE = 1;
+  // INTCONbits.IOCIE = 1;
+  // INTCONbits.GIE = 1;
 }
 
 // ====================
 // RGB helper functions
 // ====================
 
-void blue_brightness(uint8_t b)
-{
-    sw_pwm_duty = (b * 4);
-}
+void blue_brightness(uint8_t b) { sw_pwm_duty = (b * 4); }
 
-void red_brightness(uint8_t b)
-{
-    PWM2DCH = (b * 4);
-}
+void red_brightness(uint8_t b) { PWM2DCH = (b * 4); }
 
-void green_brightness(uint8_t b)
-{
-    PWM1DCH = (b * 4);
-}
+void green_brightness(uint8_t b) { PWM1DCH = (b * 4); }
 
 // ===================
 // Animation variables
@@ -131,10 +118,10 @@ uint8_t changed = 0;
  * t: 'On' time period. from 100ms to 1500ms
  */
 struct instruction_t {
-    uint8_t t : 4;
-    uint8_t b : 4;
-    uint8_t g : 4;
-    uint8_t r : 4;
+  uint8_t t : 4;
+  uint8_t b : 4;
+  uint8_t g : 4;
+  uint8_t r : 4;
 };
 
 /* animation_t is used for storing whole animations, so the intepreter
@@ -143,8 +130,8 @@ struct instruction_t {
  * start_ptr: pointer to the start of an animation
  */
 struct animation_t {
-    uint8_t size;
-    const struct instruction_t *start_ptr;
+  uint8_t size;
+  const struct instruction_t *start_ptr;
 };
 
 // ==========
@@ -171,8 +158,7 @@ const struct instruction_t heart_src[12] = {
     {0b0001, 0b0000, 0b0000, 0b0100}, {0b0010, 0b0000, 0b0000, 0b1001},
     {0b0010, 0b0000, 0b0000, 0b1001}, {0b0001, 0b0000, 0b0000, 0b0110},
     {0b0001, 0b0000, 0b0000, 0b0100}, {0b0001, 0b0000, 0b0000, 0b0011},
-    {0b0100, 0b0000, 0b0000, 0b0010}, {0b0001, 0b0000, 0b0000, 0b0001}
-};
+    {0b0100, 0b0000, 0b0000, 0b0010}, {0b0001, 0b0000, 0b0000, 0b0001}};
 const struct animation_t heart = {12, &heart_src};
 
 // ~flowing~  r->g->b->r->...
@@ -341,81 +327,78 @@ const struct animation_t trikolora = {48, &trikolora_src};
 
 // from here are the animations pulled from
 const struct animation_t *animations[COUNT_ANIMATIONS] = {&liquid, &trikolora,
-          &heart
-};
+                                                          &heart};
 
-void __interrupt() isr(void)
-{
+void __interrupt() isr(void) {
 
-    if (IOCAFbits.IOCAF3) {
-        IOCAFbits.IOCAF3 = 0;
+  if (IOCAFbits.IOCAF3) {
+    IOCAFbits.IOCAF3 = 0;
 
-        // advancing animations
-        if (animation_index == COUNT_ANIMATIONS - 1) {
-            animation_index = 0;
-        } else {
-            animation_index++;
-        }
-        changed = 1;
-
-        // hard debounce delay
-        __delay_ms(300);
-        IOCAFbits.IOCAF3 = 0;
-
+    // advancing animations
+    if (animation_index == COUNT_ANIMATIONS - 1) {
+      animation_index = 0;
     } else {
-        PIR1bits.TMR2IF = 0;
-
-        // SW PWM
-        if (sw_pwm_counter == 0) {
-            sw_pwm_counter = PWM_RESOLUTION;
-            return;
-        }
-        if (sw_pwm_counter == PWM_RESOLUTION) {
-            PORTAbits.RA2 = 1;
-        } else if (sw_pwm_counter == sw_pwm_duty) {
-            PORTAbits.RA2 = 0;
-        }
-        sw_pwm_counter--;
+      animation_index++;
     }
+    changed = 1;
+
+    // hard debounce delay
+    __delay_ms(300);
+    IOCAFbits.IOCAF3 = 0;
+
+  } else {
+    PIR1bits.TMR2IF = 0;
+
+    // SW PWM
+    if (sw_pwm_counter == 0) {
+      sw_pwm_counter = PWM_RESOLUTION;
+      return;
+    }
+    if (sw_pwm_counter == PWM_RESOLUTION) {
+      PORTAbits.RA2 = 1;
+    } else if (sw_pwm_counter == sw_pwm_duty) {
+      PORTAbits.RA2 = 0;
+    }
+    sw_pwm_counter--;
+  }
 }
 
-void main(void)
-{
+void main(void) {
 
-    // Configure oscillator to 2MHz
-    OSCCON = (0b100 << 4);
+  // Configure oscillator to 2MHz
+  OSCCON = (0b100 << 4);
 
-    // init gpio
-    PORTA = 0x0;
-    LATA = 0x0;
-    ANSELA = 0x0;
+  // init gpio
+  PORTA = 0x0;
+  LATA = 0x0;
+  ANSELA = 0x0;
 
-    pwm_init();
-    interrupt_init();
+  pwm_init();
+  interrupt_init();
 
-    while (1) {
-        // animation interpreter
-        for (uint8_t i = 0; i != animations[animation_index]->size; i++) {
-            struct instruction_t *current =
-                    &animations[animation_index]->start_ptr[i];
+  while (1) {
+    // animation interpreter
+    for (uint8_t i = 0; i != animations[animation_index]->size; i++) {
+      struct instruction_t *current =
+          &animations[animation_index]->start_ptr[i];
 
-            red_brightness(current->r);
-            green_brightness(current->g);
-            blue_brightness(current->b);
+      red_brightness(current->r);
+      green_brightness(current->g);
+      blue_brightness(current->b);
 
-            // 30, cuz otherwise it's not 100ms (interrupts make delay slower)
-            uint8_t d = (current->t);
-            for (uint8_t x = 0; x != d; x++)
-                __delay_ms(30);
+      // 30, cuz otherwise it's not 100ms (interrupts make delay slower)
+      uint8_t d = (current->t);
+      for (uint8_t x = 0; x != d; x++)
+        __delay_ms(30);
 
-            if (changed) {
-                changed = 0;
-                break;
-            }
-        }
-        if (animation_index == COUNT_ANIMATIONS - 1) {
-            SLEEP();
-            NOP();
-        }
+      if (changed) {
+        changed = 0;
+        break;
+      }
     }
+    if (animation_index == COUNT_ANIMATIONS - 1) {
+      SLEEP();
+      NOP();
+    }
+  }
 }
